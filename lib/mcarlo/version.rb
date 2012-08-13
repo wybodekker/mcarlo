1431482f8d530c9fc7714fa145ec6315e645355e
wybo <devnull@localhost>
2012-08-13 17:03:15 +0200

    creation

---
+++ lib/mcarlo/version.rb
@@ -0,0 +1,9 @@
+# Sets constant Mcarlo::VERSION, the version number of
+# this package. It is used in Gem creation but can also be consulted after
+# require'ing 'mcarlo'.
+module Mcarlo
+  # The current version of this package
+  VERSION = '2.08'
+end
+
+#$Id$
