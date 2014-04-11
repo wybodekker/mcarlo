0d3936301f46258e70fd3d2b894fe51d234ff0ec
Wybo Dekker <wybo@xs4all.nl>
2014-04-11 15:20:52 +0200

    new input format with input instead of options; converted from gem to stand-alone script

--- lib/mcarlo/version.rb
+++
@@ -1,7 +0,0 @@
-# Sets constant Mcarlo::VERSION, the version number of
-# this package. It is used in Gem creation but can also be consulted after
-# require'ing 'mcarlo'.
-module Mcarlo
-  # The current version of this package
-  VERSION = '3.0'
-end
